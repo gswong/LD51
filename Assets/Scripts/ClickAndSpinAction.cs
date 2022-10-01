41b68e58529d4595b00e0dec631978bd045d7208
Gary Wong <gswong@gmail.com>
2022-10-01 13:02:27 -0700

    spin to win

--- Assets/Scripts/ClickAndSpinAction.cs
+++ Assets/Scripts/ClickAndSpinAction.cs
@@ -4,30 +4,63 @@ using UnityEngine;
 
 public class ClickAndSpinAction : MonoBehaviour
 {
+    private float _sensitivity;
+    public Vector3 _mouseReference;
+    public Vector3 _mouseOffset;
+    public Vector3 _objectPos;
+    private Vector3 _rotation;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _sensitivity = 0.4f;
+        _rotation = Vector3.zero; 
     }
 
     // Update is called once per frame
     void Update()
     {
-
     }
 
     void OnMouseDown()
     {
-        Debug.Log("Test mouse down");
+        Debug.Log("Mouse clicked cauldron");
+         
+        // store mouse
+        _mouseReference = Input.mousePosition;
     }
 
     void OnMouseUp()
     {
-        Debug.Log("Test mouse up");
+        Debug.Log("Mouse released cauldron");
     }
 
     void OnMouseDrag()
     {
-        Debug.Log("Test mouse drag");
+        Debug.Log("Mouse click and rotate cauldron");
+
+        _mouseOffset = (Input.mousePosition - _mouseReference);
+        _objectPos = Camera.main.WorldToScreenPoint(transform.position);
+
+        // flip rotation relative to position of game object
+        float flipx = 1;
+        float flipy = 1;
+        if (Input.mousePosition.y < _objectPos.y)
+        {
+            flipx = -1;
+        }
+        if (Input.mousePosition.x < _objectPos.x)
+        {
+            flipy = -1;
+        }
+        
+        // apply rotation
+        _rotation.z = -(_mouseOffset.x * flipx - _mouseOffset.y * flipy) * _sensitivity;
+        
+        // rotate
+        transform.Rotate(_rotation);
+        
+        // store mouse
+        _mouseReference = Input.mousePosition;
     }
 }
