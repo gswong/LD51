8a52eb9157311601d0dcf54a9eb2c88afc6029bf
Gary Wong <gswong@gmail.com>
2022-10-02 17:17:57 -0700

    Add UI stuff

--- Assets/Scripts/ResourceManager.cs
+++ Assets/Scripts/ResourceManager.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 [RequireComponent(typeof(ResourceManager))]
 public class ResourceManager : Singleton<ResourceManager>
@@ -18,17 +20,33 @@ public class ResourceManager : Singleton<ResourceManager>
     public int ScoreValue;
     public int ProgressValue;
 
+    public TMP_Text EyeText;
+    public TMP_Text MushroomText;
+    public TMP_Text RootText;
+    public Canvas ResourceCanvas;
+
     public enum ResourceType { Eye, Mushroom, Root }
 
     void Start()
     {
+        EyeText = GameObject.FindGameObjectWithTag("EyeText").GetComponent<TMP_Text>();
+        MushroomText = GameObject.FindGameObjectWithTag("MushroomText").GetComponent<TMP_Text>();
+        RootText = GameObject.FindGameObjectWithTag("RootText").GetComponent<TMP_Text>();
         Interlocked.Exchange(ref ResourceEyeValue, 0);
         Interlocked.Exchange(ref ResourceMushroomValue, 0);
         Interlocked.Exchange(ref ResourceRootValue, 0);
         Interlocked.Exchange(ref ResourceEyeValueForCat, 0);
         Interlocked.Exchange(ref ResourceMushroomValueForCat, 0);
         Interlocked.Exchange(ref ResourceRootValueForCat, 0);
+        ResourceCanvas = GameObject.FindGameObjectWithTag("ResourceCanvas").GetComponent<Canvas>();
+        ResourceCanvas.enabled = false;
+        GameManager.GameStartEvent += EnableCanvas;
         SetDontDestroy();
+    }
+
+    public void EnableCanvas()
+    {
+        ResourceCanvas.enabled = true;
     }
 
     public void IncreaseScore()
@@ -170,6 +188,17 @@ public class ResourceManager : Singleton<ResourceManager>
         if (regainResourceAction.Count > 0)
         {
             regainResourceAction[UnityEngine.Random.Range(0, regainResourceAction.Count)]();
+        }
+    }
+
+    void Update()
+    {
+        // update ui
+        if (ResourceCanvas.enabled)
+        {
+            EyeText.text = ResourceEyeValue.ToString(); 
+            MushroomText.text = ResourceMushroomValue.ToString(); 
+            RootText.text = ResourceRootValue.ToString(); 
         }
     }
 
